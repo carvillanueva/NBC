30effbe8625786978e2bcb5cf356c65589d724e8
Carlos Villanueva <cvillanueva@myafo.com>
2023-05-18 20:55:03 -0500

    update about

--- src/app/home/about.component.ts
+++ src/app/home/about.component.ts
@@ -8,34 +8,20 @@ import { Component } from '@angular/core';
     `,
   ],
   template: `
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div class="col-sm-8 col-lg-6">
-          <img
-            src="../assets/images/bootstrap-themes.png"
-            class="d-block mx-lg-auto img-fluid"
-            alt="Bootstrap Themes"
-            width="700"
-            height="500"
-            loading="lazy"
-          />
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+      <div class="p-3">
+        <h1 class="display-4 fw-bold lh-1 text-body-emphasis">About Nora's Cleaning Service</h1>
+        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Primary</button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
         </div>
-        <div class="col-lg-6">
-          <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
-            Responsive left-aligned hero with image
-          </h1>
-          <p class="lead">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
-          </p>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" class="btn contactBtn text-white btn-lg px-4 me-md-2">
-              Primary
-            </button>
-          </div>
-        </div>
+      </div>
+      <!-- <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          <img class="rounded-lg-3" src="bootstrap-docs.png" alt="" width="720">
+      </div> -->
     </div>
+
   `,
 })
 export class AboutSectionComponent {}
